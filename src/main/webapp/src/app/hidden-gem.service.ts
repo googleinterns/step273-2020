7f7683bc89758189239e991f122df531688fa045
Helena <helena.e.tran@student.uts.edu.au>
2021-01-20 23:29:26 +0000

    Write tests for hidden gem service

--- src/main/webapp/src/app/hidden-gem.service.ts
+++ src/main/webapp/src/app/hidden-gem.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { HiddenGem } from './hidden-gem';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,12 @@ export class HiddenGemService {
   constructor(private httpClient: HttpClient) { }
 
   getAllHiddenGems() : Observable<HiddenGem[]> {
-    return this.httpClient.get<HiddenGem[]>('/hiddengems')
+    return this.httpClient
+      .get<HiddenGem[]>('/hiddengems')
+      .pipe(catchError(this.handleError))
+  }
+
+  handleError(error: HttpErrorResponse) {
+    return throwError('A data error occured, please try again.');
   }
 }
