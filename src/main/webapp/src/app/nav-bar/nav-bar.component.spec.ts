49120c4382b2bec77759bc0aa90404d5d4c71d94
Jamila Sabazova <zh.sabazova02@gmail.com>
2021-01-21 05:48:44 +0000

    final test with routiung done

--- src/main/webapp/src/app/nav-bar/nav-bar.component.spec.ts
+++ src/main/webapp/src/app/nav-bar/nav-bar.component.spec.ts
@@ -1,24 +1,66 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async, inject } from '@angular/core/testing';
 import { NavBarComponent } from './nav-bar.component';
 
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { By } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MapComponent } from '../map/map.component';
+
+
 describe('NavBarComponent', () => {
+ 
   let component: NavBarComponent;
   let fixture: ComponentFixture<NavBarComponent>;
-
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavBarComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ NavBarComponent ],
+      providers: [
+        NavBarComponent
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        RouterTestingModule.withRoutes([
+         { path: '', component: MapComponent }
+        ])
+      ],
+    }),
+   
     fixture = TestBed.createComponent(NavBarComponent);
     component = fixture.componentInstance;
+    
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create a NavBar', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  it('should go to home url',
+    inject([Router, Location], (router: Router, location: Location)=> {
+    let fixture = TestBed.createComponent(NavBarComponent);
+    fixture.detectChanges();
+
+    fixture.debugElement.query(By.css('a')).nativeElement.click();
+    fixture.whenStable().then(() => {
+      expect(location.path()).toEqual('/');
+      console.log('after expect');
+    });
+  }));
+
+  it('routes should be rendered correctly', 
+    inject([Router, Location], (router: Router, location: Location)=> {
+      let href = fixture.debugElement.query(By.css('a')).nativeElement
+      .getAttribute('href');
+    expect(href).toEqual('/recommendation');
+  }));
+})
--- src/main/webapp/src/app/nav-bar/nav-bar.component.ts
+++ src/main/webapp/src/app/nav-bar/nav-bar.component.ts
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class NavBarComponent implements OnInit {
+  [x: string]: any;
 
-  navigatedAway: boolean = false;
+
   constructor() {  }
   ngOnInit(): void { 
   
