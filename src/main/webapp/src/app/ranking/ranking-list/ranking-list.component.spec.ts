598ea2abc6ffdfd801a42d9afcfd93a1cc6d26d5
Helena <helena.e.tran@student.uts.edu.au>
2021-01-22 02:39:35 +0000

    Add test to limit to only 10 hidden gems for ranking

--- src/main/webapp/src/app/ranking/ranking-list/ranking-list.component.spec.ts
+++ src/main/webapp/src/app/ranking/ranking-list/ranking-list.component.spec.ts
@@ -4,7 +4,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 
 import { RankingListComponent } from './ranking-list.component';
 import { HiddenGemService } from '../../hidden-gem.service';
-
+import { HiddenGem } from '../../hidden-gem';
 
 describe('HiddenGemService', () => {
 
@@ -22,5 +22,35 @@ describe('HiddenGemService', () => {
     const service: HiddenGemService = TestBed.get(HiddenGemService);
     expect(service.getAllHiddenGems).toBeTruthy();
     });
-
 });
+
+//TODO:get rid of the HTTP client problem by,maybe,having the first describe go first, then the second one? Or include the service in the test, or combine
+describe('RankingListComponent', () => {
+  let component: RankingListComponent;
+  let fixture: ComponentFixture<RankingListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RankingListComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [HiddenGemService]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RankingListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should display only 10 hidden gems', () => {
+    const fixture = TestBed.createComponent(RankingListComponent);
+    const component = fixture.componentInstance;
+    const hiddenGems = new Array<HiddenGem>(20);
+    component.hiddenGems = hiddenGems;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelectorAll('app-ranking-item').length).toEqual(10);
+  })
+})
