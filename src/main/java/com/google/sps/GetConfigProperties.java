43670ffa8514952694530108663c8662c4df5532
Helena <helena.e.tran@student.uts.edu.au>
2021-01-28 22:51:06 +0000

    Update getApiKey function and write javadoc for functions

--- src/main/java/com/google/sps/GetConfigProperties.java
+++ src/main/java/com/google/sps/GetConfigProperties.java
@@ -5,7 +5,16 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+/** This class consists of functions to retrieve the properties from app.properties. */
 public class GetConfigProperties {
+  /** This function is used to get the value of a property from a given 
+   * properties file and the property name.
+   * @param filepath                The name of the file from which we want to get the property value.
+   * @param propertyName            The name of the property for which we want to get the value. 
+   * @return String                 This returns the String value of the property.
+   * @throws FileNotFoundException  This exception is thrown if the filepath given does not exist. 
+   * @throws IOException            This exception is thrown if the filepath and/or the propertyName do not exist. 
+   */
   public static String getPropertyValue(String filepath, String propertyName) throws FileNotFoundException, IOException {
     String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
     String appConfigPath = rootPath + filepath;
@@ -21,18 +30,13 @@ public class GetConfigProperties {
       return propertyValue;
   }
 
+  /**
+   * This function retrieves the value of the API Key from the app.properties file. 
+   * @return String                 This return the String value of the API key.
+   * @throws FileNotFoundException  This exception is thrown if the filepath given does not exist. 
+   * @throws IOException            This exception is thrown if the filepath and/or the propertyName do not exist. 
+   */
   public static String getApiKey() throws FileNotFoundException, IOException {
-    String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-    String appConfigPath = rootPath + "app.properties";
-
-    Properties appProps = new Properties();
-    appProps.load(new FileInputStream(appConfigPath));
-
-    String apiKey = appProps.getProperty("api_key");
-    if (apiKey == null) {
-      return "";
-    }
-    else
-      return apiKey;
+    return getPropertyValue("app.properties", "api_key");
   }
 }
