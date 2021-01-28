6c7c73586dde486eb803c36cb06173370292d3bc
sehhajdkg <sehhajdkg@gmail.com>
2021-01-28 23:49:41 +0000

    junit test working now

--- src/test/java/com/google/sps/GetTop3GemsTest.java
+++ src/test/java/com/google/sps/GetTop3GemsTest.java
@@ -25,10 +25,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import com.google.sps.servlets.GetTop3GemsServlet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
+import com.google.sps.data.HiddenGem;
+import com.google.sps.data.HiddenGems;
 
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
+
 
 @RunWith(JUnit4.class)
 public final class GetTop3GemsTest {
@@ -47,43 +55,16 @@ public final class GetTop3GemsTest {
 
       gemsServlet.doGet(request, response);
       
-      //System.out.print(servletResponse.toString());
-
-      // String expected = "[
-      //   {\"id\":1,
-      //   \"name\":\"Thomas St. Kitchen \u0026 Bar\",
-      //   \"business_type\":\"restaurant\",
-      //   \"address\":\"169-179 Thomas St, Sydney NSW 2000\",
-      //   \"price_level\":1,
-      //   \"rating\":4.2,
-      //   \"photo\":\"https://cdn.pixabay.com/photo/2015/09/02/12/43/meal-918639_960_720.jpg\"},
-      //   {\"id\":2,
-      //   \"name\":\"Fratelli Fresh\",
-      //   \"business_type\":\"restaurant\",
-      //   \"address\":\"ICC Sydney, tenancy 2/14 Darling Dr, Sydney NSW 2000\",
-      //   \"price_level\":2,
-      //   \"rating\":3.6,
-      //   \"photo\":\"https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg\"},
-      //   {\"id\":3,
-      //   \"name\":\"Kana Sushi Leichhardt\",
-      //   \"business_type\":\"restaurant\",
-      //   \"address\":\"53-57 Norton St, Leichhardt NSW 2040\",
-      //   \"price_level\":1,
-      //   \"rating\":5.0,
-      //   \"photo\":\"https://cdn.pixabay.com/photo/2018/08/03/08/33/food-3581341_960_720.jpg\"}]\"";
-
-       // String expected = "[{\"id\":1,\"name\":\"Thomas St. Kitchen \u0026 Bar\",\"business_type\":\"restaurant\",\"address\":\"169-179 Thomas St, Sydney NSW 2000\","\"price_level\":1,\"rating\":4.2,\"photo\":\"https://cdn.pixabay.com/photo/2015/09/02/12/43/meal-918639_960_720.jpg\"},{\"id\":2,\"name\":\"Fratelli Fresh\",\"business_type\":\"restaurant\",\"address\":\"ICC Sydney, tenancy 2/14 Darling Dr, Sydney NSW 2000\",\"price_level\":2,\"rating\":3.6,\"photo\":\"https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg\"},{\"id\":3,\"name\":\"Kana Sushi Leichhardt\",\"business_type\":\"restaurant\",\"address\":\"53-57 Norton St, Leichhardt NSW 2040\",\"price_level\":1,\"rating\":5.0,\"photo\":\"https://cdn.pixabay.com/photo/2018/08/03/08/33/food-3581341_960_720.jpg\"}]";
       String expected = "[{\"id\":1,\"name\":\"Thomas St. Kitchen \\u0026 Bar\",\"business_type\":\"restaurant\",\"address\":\"169-179 Thomas St, Sydney NSW 2000\",\"price_level\":1,\"rating\":4.2,\"photo\":\"https://cdn.pixabay.com/photo/2015/09/02/12/43/meal-918639_960_720.jpg\"},"
       +"{\"id\":2,\"name\":\"Fratelli Fresh\",\"business_type\":\"restaurant\",\"address\":\"ICC Sydney, tenancy 2/14 Darling Dr, Sydney NSW 2000\",\"price_level\":2,\"rating\":3.6,\"photo\":\"https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg\"},"
-      +"{\"id\":3,\"name\":\"Kana Sushi Leichhardt\",\"business_type\":\"restaurant\",\"address\":\"53-57 Norton St, Leichhardt NSW 2040\",\"price_level\":1,\"rating\":5.0,\"photo\":\"https://cdn.pixabay.com/photo/2018/08/03/08/33/food-3581341_960_720.jpg\"}]";
+      +"{\"id\":3,\"name\":\"Kana Sushi Leichhardt\",\"business_type\":\"restaurant\",\"address\":\"53-57 Norton St, Leichhardt NSW 2040\",\"price_level\":1,\"rating\":5.0,\"photo\":\"https://cdn.pixabay.com/photo/2018/08/03/08/33/food-3581341_960_720.jpg\"}]\n";
 
-
-      Gson gson = new Gson();
-      String jsonResponse = gson.toJson(servletResponse.toString());
-      System.out.print(servletResponse.toString());
-      System.out.print(expected);
-      System.out.print(jsonResponse);
-      //assertTrue(servletResponse.toString().equals(expected));
-      
+ 
+      Gson gson = new Gson(); 
+      Type responseListType = new TypeToken<ArrayList<HiddenGem>>(){}.getType();
+      ArrayList<HiddenGem> responseArray = gson.fromJson(servletResponse.toString(), responseListType); 
+      assertTrue(responseArray.size() == 3);
+      assertTrue(servletResponse.toString().equals(expected));
+         
     }
 }
