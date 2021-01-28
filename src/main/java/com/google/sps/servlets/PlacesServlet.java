44f7315c27f6d700ea6184717180da3dce525d1f
Helena <helena.e.tran@student.uts.edu.au>
2021-01-28 01:06:48 +0000

    Retrieve 60 results for each type: restaurant and cafe

--- src/main/java/com/google/sps/servlets/PlacesServlet.java
+++ src/main/java/com/google/sps/servlets/PlacesServlet.java
@@ -24,6 +24,8 @@ import com.google.maps.model.PlacesSearchResponse;
 import com.google.maps.model.RankBy;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,7 +34,6 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that return the hidden gems dummy data. */
 @WebServlet("/places")
 public class PlacesServlet extends HttpServlet {
-  private final int radius = 10000;
 
   GeoApiContext context = new GeoApiContext.Builder()
     .apiKey("myapikey")
@@ -42,33 +43,37 @@ public class PlacesServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    PlacesSearchResponse restaurant_results = new PlacesSearchResponse();
-    try {
-      restaurant_results = PlacesApi.nearbySearchQuery(context, location)
-        .radius(radius)
-        .rankby(RankBy.PROMINENCE)
-        .type(PlaceType.RESTAURANT)
-        .await();
-    } catch (ApiException | InterruptedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-
-    PlacesSearchResponse cafes_results = new PlacesSearchResponse();
-    try {
-      cafes_results = PlacesApi.nearbySearchQuery(context, location)
-        .radius(radius)
-        .rankby(RankBy.PROMINENCE)
-        .type(PlaceType.CAFE)
-        .await();
-    } catch (ApiException | InterruptedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-
     Gson gson = new Gson();
-    String jsonResponse = gson.toJson(restaurant_results);
-    jsonResponse = jsonResponse.concat(gson.toJson(cafes_results));
+    PlacesSearchResponse restaurant_results = new PlacesSearchResponse();
+    PlacesSearchResponse cafes_results = new PlacesSearchResponse();
+    String jsonResponse = gson.toJson("");
+    String restaurantNextPageToken = "";
+    String cafeNextPageToken = "";
+
+    for (int i = 0; i < 3; i++) {
+      try {
+        restaurant_results = PlacesApi.nearbySearchQuery(context, location)
+          .rankby(RankBy.DISTANCE)
+          .type(PlaceType.RESTAURANT)
+          .pageToken(restaurantNextPageToken)
+          .await();
+        
+          cafes_results = PlacesApi.nearbySearchQuery(context, location)
+          .rankby(RankBy.DISTANCE)
+          .type(PlaceType.CAFE)
+          .pageToken(cafeNextPageToken)
+          .await();
+        
+          TimeUnit.SECONDS.sleep(2);
+        restaurantNextPageToken = restaurant_results.nextPageToken;
+        cafeNextPageToken = cafes_results.nextPageToken;
+      } catch (ApiException | InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+      jsonResponse = jsonResponse.concat(gson.toJson(restaurant_results));
+      jsonResponse = jsonResponse.concat(gson.toJson(cafes_results));
+    }
 
     // Send the JSON back as the response
     response.setContentType("application/json");
