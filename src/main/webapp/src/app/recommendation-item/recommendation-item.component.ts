3f30b7f132b8d575246b1d6ded278d58fe0b100c
Helena <helena.e.tran@student.uts.edu.au>
2021-02-08 23:08:41 +0000

    Update recommendations cards

--- src/main/webapp/src/app/recommendation-item/recommendation-item.component.ts
+++ src/main/webapp/src/app/recommendation-item/recommendation-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { HiddenGem } from '../hidden-gem';
 
 
@@ -7,7 +7,18 @@ import { HiddenGem } from '../hidden-gem';
   templateUrl: './recommendation-item.component.html',
   styleUrls: ['./recommendation-item.component.css']
 })
-export class RecommendationItemComponent {
+export class RecommendationItemComponent implements OnInit{
   @Input() hiddenGem = {} as HiddenGem;
+  photoUrl!: string;
 
+  ngOnInit() {
+    if (this.hiddenGem.photos != null) {
+      this.photoUrl = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference="
+        + this.hiddenGem.photos[0].photoReference
+        + "&key=AIzaSyCBb8QQBQal9jDNl3ZG6f3bS6ROX2MtYIM";
+    }
+    else {
+      this.photoUrl = "https://www.flaticon.com/svg/vstatic/svg/3716/3716538.svg?token=exp=1612753174~hmac=f38de8989e705031bdecb2da1464c379"
+    }
+  }
 }
