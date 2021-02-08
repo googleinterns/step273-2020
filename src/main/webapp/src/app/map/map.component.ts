ea163374509bba01eba1f06f8e6c8592672b0ce7
Jamila Sabazova <jamilasabazova@google.com>
2021-02-08 05:05:53 +0000

    2 markers

--- src/main/webapp/src/app/map/map.component.ts
+++ src/main/webapp/src/app/map/map.component.ts
@@ -12,6 +12,7 @@ export class MapComponent implements AfterViewInit {
 
   @ViewChild("mapContainer", { static: false })
   mapContainer!: ElementRef;
+  
   map!: google.maps.Map;
 
   location = {} as Location;
@@ -37,46 +38,30 @@ export class MapComponent implements AfterViewInit {
       zoom: 10
     };
     this.map = new google.maps.Map(this.mapContainer.nativeElement, mapOptions);
-    
-    //Default Marker
-    const marker = new google.maps.Marker({
-      position: centerOfMapCoordinates,
-      map: this.map,
-      title: "Hello World!"
-    });
-    // const markers = [
-    //   { },
-    //   { }
-    // ];
-    const markers = [
-      {
-        position: new google.maps.LatLng(40.73061, 73.935242),
-        map: this.map,
-        title: "Marker 1"
-      },
-      {
-        position: new google.maps.LatLng(32.06485, 34.763226),
-        map: this.map,
-        title: "Marker 2"
-      }
-    ];
-    //Adding Click event to default marker
-    marker.addListener("click", () => {
-      const infoWindow = new google.maps.InfoWindow({
-        content: "My first marker."
-      });
-      infoWindow.open(this.map, marker);
-    });
-
-    //Adding default marker to map
-    marker.setMap(this.map);
 
     //Adding other markers
-    // this.loadAllMarkers();
-  // }
+     this.loadAllMarkers();
+  }
 
 
-    // loadAllMarkers(): void {
+  loadAllMarkers(): void {
+   
+    const markersJson = [
+      { },
+      { }
+    ];
+    const markers = [
+      {
+        position: new google.maps.LatLng( -33.513059, 151.1234),
+        map: this.map,
+        title: "Quay restaurant"
+      },
+      {
+        position: new google.maps.LatLng(-33.47,151.865),
+        map: this.map,
+        title: "Food Van cafe"
+      }
+    ];
 
     markers.forEach(markerInfo => {
       // //Creating a new marker object
