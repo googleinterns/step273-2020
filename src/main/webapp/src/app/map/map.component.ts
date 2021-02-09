53c4154231957f1712c51fe4193680cda7774613
Jamila Sabazova <jamilasabazova@google.com>
2021-02-09 06:03:57 +0000

    loop with info window

--- src/main/webapp/src/app/map/map.component.ts
+++ src/main/webapp/src/app/map/map.component.ts
@@ -2,6 +2,8 @@ import { Component, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
 import { LocationService } from 'src/app/location.service';
 import { Location } from 'src/app/location';
 import { HiddenGemService } from '../hidden-gem.service';
+import { HiddenGem } from 'src/app/hidden-gem';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-map',
@@ -14,6 +16,7 @@ export class MapComponent implements AfterViewInit {
   @ViewChild("mapContainer", { static: false })
   mapContainer!: ElementRef;
   map!: google.maps.Map;
+  hiddenGems! : HiddenGem[];
   location = {} as Location;
 
   constructor(private locationService: LocationService) { 
@@ -39,38 +42,32 @@ export class MapComponent implements AfterViewInit {
   }
 
   loadMarkers(): void {
-    // Hard-coded dummy markers.
-    const markers = [
-      {
-        position: new google.maps.LatLng( -33.513059, 151.1234),
+    // Fetch data from json string from Hidden Gems object.
+
+    // Creating a global infoWindow object that will be reused by all markers
+		var infoWindow = new google.maps.InfoWindow();
+
+    for (var i = 0, length = this.hiddenGems.length; i < length; i++) {
+      var data = this.hiddenGems[i],
+      latLng = new google.maps.LatLng( -33.513059, 151.1234+i);//(data.geometry.lat, data.geometry.lng); 
+
+      // Creating a marker and putting it on the map
+      var marker = new google.maps.Marker({
+        position: latLng,
         map: this.map,
-        title: "name: Quay restaurant, address: 123 Sydney Street, Sydney, star rating: 3"
-      },
-      {
-        position: new google.maps.LatLng(-33.47,151.865),
-        map: this.map,
-        title: "name: Food Van cafe, address: 123 Sydney Street, Sydney, star rating: 3"
-      }
-    ];
-
-    markers.forEach(markerInfo => {
-      // Creating a new marker object.
-      const markerObj = new google.maps.Marker({
-        ...markerInfo
+        title: data.name + data.address + 
+          "star rating:" + data.rating
       });
-
-      // Creating a new info window with markers info.
-      const infoWindow = new google.maps.InfoWindow({
-        content: markerInfo.title,
-      });
-
-      // Add click event to open info window on marker.
-      markerObj.addListener("click", () => {
-        infoWindow.open(markerInfo.map, markerObj);
-      });
-
-      // Adding marker to google map.
-      markerObj.setMap(this.map);
-    });
+    
+    	// Creating a closure to retain the correct data, notice how I pass the current data in the loop into the closure (marker, data)
+			(function(marker, data) {
+				// Attaching a click event to the current marker
+				google.maps.event.addListener(marker, "click", function(e) {
+					infoWindow.setContent(data.name);
+					infoWindow.open(this.map, marker);
+				});
+      })
+      (marker, data);
+		}
   }
 }
