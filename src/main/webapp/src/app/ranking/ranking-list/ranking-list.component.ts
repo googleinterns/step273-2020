c33c444c1a96a1c59dd42688d2d315316a25233a
Helena <helena.e.tran@student.uts.edu.au>
2021-02-12 08:27:50 +0000

    frontend ranking filtering working

--- src/main/webapp/src/app/ranking/ranking-list/ranking-list.component.ts
+++ src/main/webapp/src/app/ranking/ranking-list/ranking-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 import { LocationService } from '../../location.service';
 import { Location } from 'src/app/models/location';
+import { HiddenGem } from 'src/app/models/hidden-gem';
 
 
 @Component({
@@ -12,7 +13,10 @@ import { Location } from 'src/app/models/location';
 export class RankingListComponent implements OnInit {
 
   hiddenGems = this.appComponent.hiddenGems;
+  restaurants = [] as HiddenGem[];
+  cafes = [] as HiddenGem[];
   location = {} as Location;
+  show = "any";
 
   constructor(private locationService: LocationService, private appComponent: AppComponent) { }
 
@@ -26,6 +30,12 @@ export class RankingListComponent implements OnInit {
   ngDoCheck() {
     if (this.hiddenGems !== this.appComponent.hiddenGems) {
       this.hiddenGems = this.appComponent.hiddenGems;
+      this.restaurants = this.hiddenGems.filter(hiddenGem => hiddenGem.types[0] === "restaurant");
+      this.cafes = this.hiddenGems.filter(hiddenGem => hiddenGem.types[0] === "cafe");
     }
+  }
+
+  updateShow(showType: string) {
+    this.show = showType;
   }
 }
