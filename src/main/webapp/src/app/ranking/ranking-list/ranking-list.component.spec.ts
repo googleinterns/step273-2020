1b0c9489482c5003717cc2975eede1ed070ec845
Helena <helena.e.tran@student.uts.edu.au>
2021-02-12 03:57:46 +0000

    Update tests

--- src/main/webapp/src/app/ranking/ranking-list/ranking-list.component.spec.ts
+++ src/main/webapp/src/app/ranking/ranking-list/ranking-list.component.spec.ts
@@ -6,6 +6,8 @@ import { HiddenGemService } from '../../hidden-gem.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { LocationService } from '../../location.service';
 import { Location } from 'src/app/models/location';
+import { HiddenGem } from 'src/app/models/hidden-gem';
+import { By } from '@angular/platform-browser';
 
 describe('RankingListComponent', () => {
   let component: RankingListComponent;
@@ -50,14 +52,14 @@ describe('RankingListComponent', () => {
     expect(component.location).toEqual(defaultSydneyLocation);
   });
 
-  it('should display the spinner only when fetching', () => {
+  it('should display the spinner when hidden gems are not fetched yet', () => {
+    const hiddenGems: HiddenGem[] = []
+    component.hiddenGems = hiddenGems;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    if (!component.hiddenGems) {
-      expect(compiled.querySelector('mat-spinner').src).toBeDefined;
-    }
-    else {
-      expect(compiled.querySelector('mat-spinner').src).toBeUndefined;
-    }
+    expect(fixture.debugElement.query(By.css('mat-spinner'))).not.toBeNull();
   })
+
+  //TODO: Write test to check that the spinner is hidden when the fetch is done.
+  //Attempt: try to push a hidden gem to the array and check for toBeNull()
+  // But got error: 'Expected DebugElement__POST_R3__({ nativeNode: <mat-spinner _ngcontent-a-c53> }) to be null.'
 })
